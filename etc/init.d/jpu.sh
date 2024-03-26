968cc45dba9d959b4565ef1f62cdd9e957c9ebe8
fuqiang <qiang.fu@spacemit.com>
2024-03-26 19:19:51 +0800

    chmod /dev/jpu0 from 600 to 660

---
+++ etc/init.d/jpu.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+###
+ # Copyright 2022-2023 SPACEMIT. All rights reserved.
+ # Use of this source code is governed by a BSD-style license
+ # that can be found in the LICENSE file.
+ # 
+ # @Author: David(qiang.fu@spacemit.com)
+ # @Date: 2023-09-13 14:20:45
+ # @LastEditTime: 2023-09-20 14:04:40
+ # @Description: 
+### 
+
+chmod 660 /dev/jpu0
